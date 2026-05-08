2c471b7c9864296b0744fc4f56e08908439eb4e4
Muhammad Saad Sohail <msaad.bahria@gmail.com>
2026-05-08 21:16:20 +0500

    Update graph_service.py

--- backend/project/services/graph_service.py
+++ backend/project/services/graph_service.py
@@ -1,6 +1,20 @@
-graph = {
-    "W": {"DHA": 200, "Clifton": 180, "Saddar": 100},
-    "DHA": {"Clifton": 50},
-    "Clifton": {"Saddar": 80},
-    "Saddar": {}
+from .map_api_service import fetch_graph
+
+
+EXAMPLE_GRAPH = {
+    "Warehouse": {
+        "DHA": {"distance": 20, "time": 30, "cost": 200},
+        "Clifton": {"distance": 18, "time": 25, "cost": 180},
+    },
+    "DHA": {
+        "Clifton": {"distance": 5, "time": 10, "cost": 50},
+        "Korangi": {"distance": 12, "time": 20, "cost": 120},
+    },
+    "Clifton": {
+        "Saddar": {"distance": 8, "time": 12, "cost": 80},
+    },
 }
+
+
+def build_graph(locations, provider_config=None):
+    return fetch_graph(locations, provider_config)
